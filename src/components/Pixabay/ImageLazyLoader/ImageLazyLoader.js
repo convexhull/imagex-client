893a6330c57ec72f3b781a3546fac90eed273dc2
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-07-08 15:06:09 +0530

    TYPE:FEATURE -> complete image grid display taking care of image orientation and layout

--- src/components/Pixabay/ImageLazyLoader/ImageLazyLoader.js
+++ src/components/Pixabay/ImageLazyLoader/ImageLazyLoader.js
@@ -7,6 +7,7 @@ import ImageGrid from '../ImageGrid/ImageGrid';
 import Spinner from '../../UI/Spinner/Spinner';
 import * as actions from '../../../store/actions/index';
 import classes from './ImageLazyLoader.module.css';
+import image from '../../../assets/images/logo.png';
 
 
 class ImageLazyLoader extends Component {
@@ -21,14 +22,14 @@ class ImageLazyLoader extends Component {
         let options = {
             root: null,
             rootMargin: '0px',
-            threshold: 0.9
+            threshold: 0.5
         }
         let pagecount = 0;
         let callback = (entries, observer) => {
             console.log("xxxxxxx", entries[0]);
-            if(!entries[0].isIntersecting){
-                return;
-            }
+            // if(!entries[0].isIntersecting){
+            //     return;
+            // }
             pagecount++;
             this.props.onSearchByKeyword(this.props.keyword, pagecount);
         }
@@ -38,10 +39,12 @@ class ImageLazyLoader extends Component {
 
     render(){
         let spinner = (this.props.endOfResults ? <p>That's it folks. You have reached the end of results.</p> : <Spinner />);
+        let dummyimage = [<div className={classes.Dummydiv}>Dummy image div</div>,<div className={classes.Dummydiv}>Dummy image div</div>,<div className={classes.Dummydiv}>Dummy image div</div>,<div className={classes.Dummydiv}>Dummy image div</div>];
         return (
             <div className={classes.ImageLazyLoader}>
-                <ImageGrid images={this.props.images} />
-                <div ref={this.loaderRef} >
+                {this.props.images.length ? <ImageGrid images={this.props.images} /> : dummyimage }
+                {/* <ImageGrid images={this.props.images} /> */}
+                <div ref={this.loaderRef} style={{backgroundColor: 'pink', width : '100%'}} >
                     <Spinner /> 
                 </div>
             </div>
--- src/components/Unsplash/ImageGrid/ImageGrid.js
+++ src/components/Unsplash/ImageGrid/ImageGrid.js
@@ -5,19 +5,31 @@ import classes from './ImageGrid.module.css';
 class ImageGrid extends Component {
 
     render(){
-        let images = this.props.images.map((image) => (
-          <div>
-            <img src={image.urls.small} alt="image" />
-          </div>
-        ));
-        
-        return (
-          <div>
-            <div className={classes["image-grid"]}>
-                {images}
+      let images1 = this.props.images.map( (image) => {
+        if(image.width > image.height){
+          return (
+            <div className={classes.landscape}>
+              <img src={image.urls.small} alt="image" />
             </div>
+          )
+        }
+        else {
+          return (
+            <div className={classes.portrait}>
+              <img src={image.urls.small} alt="image" />
+            </div>
+          )
+        }
+      })
+      
+
+      return (
+        <div>
+          <div className={classes["image-grid"]}>
+            {images1}
           </div>
-        );
+        </div>
+      );
     }
 
 }
