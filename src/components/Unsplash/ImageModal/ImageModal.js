bc522dc288b23de517c0c63e41d6b04caddffbc2
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-07-10 02:26:37 +0530

    TYPE:FEATURE -> improve unsplash image view modal header section

--- src/components/Unsplash/ImageModal/ImageModal.js
+++ src/components/Unsplash/ImageModal/ImageModal.js
@@ -1,7 +1,6 @@
 import React , { Component } from 'react';
 import Modal from '../../Common/UI/Modal/Modal';
 import classes from './ImageModal.module.css';
-import Spinner from '../../UI/Spinner2/Spinner2';
 
 
 class ImageModal extends Component {
@@ -16,19 +15,22 @@ class ImageModal extends Component {
     render(){
         return (
             <Modal show={this.props.show} hideModal={this.props.hideImageModal} >
-                <div className={classes["image-options"]}>
-                    <div>
-                        User's Info
+                <div className={classes["image-header"]}>
+                    <div className={classes["user-info"]}>
+                        <div>
+                            <img src="https://images.unsplash.com/profile-1529188896528-7f0ccc5a7172?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32" alt="user image" />
+                        </div>
+                        <p>Yash</p>
+                        <p>Singh</p>
                     </div>
-                    <div>
+                    <div className={classes["actions"]}>
                         Download options
                     </div>  
                 </div>
                 <div className={classes["image-container"]}>
                     <img src={this.props.imageToLoad} alt="india's tricolour flag" onLoad={this.imageLoadedHandler} />
-                    {/* {this.state.loading ? <Spinner /> : null } */}
                 </div>
-                <div>
+                <div className={classes["image-footer"]}>
                     India's tricolour
                 </div>
             </Modal>
