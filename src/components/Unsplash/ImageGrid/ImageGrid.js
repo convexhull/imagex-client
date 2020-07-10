fde6667ac870c30ad0ddc33485b285471e3fdb17
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-07-10 18:08:50 +0530

    TYPE:FEATURE -> improve unsplash image modal ui, add animations to image grid and add ionic font icons

--- src/components/Unsplash/ImageGrid/ImageGrid.js
+++ src/components/Unsplash/ImageGrid/ImageGrid.js
@@ -7,11 +7,11 @@ class ImageGrid extends Component {
 
   state = {
     showImageModal : false,
-    imageToLoad: ''
+    selectedImage: ''
   }
 
-  imageClickHandler = (imageUrl) => {
-    this.setState({showImageModal : true, imageToLoad: imageUrl})
+  imageClickHandler = (image) => {
+    this.setState({showImageModal : true, selectedImage: image})
   }
 
   hideModalHandler = () => {
@@ -23,14 +23,14 @@ class ImageGrid extends Component {
       let imgOrientation = (image.width >= image.height ? "landscape" : "portrait") ; 
       return (
         <div className={classes[imgOrientation]}>
-          <img src={image.urls.small} alt="image" onClick={() => this.imageClickHandler(image.urls.regular)} />
+          <img src={image.urls.small} alt="image" onClick={() => this.imageClickHandler(image)} />
         </div>
       );
     })
 
     return (
       <div>
-        {this.state.showImageModal ? <ImageModal show={this.state.showImageModal} hideImageModal={this.hideModalHandler} imageToLoad={this.state.imageToLoad}/> : null}
+        {this.state.showImageModal ? <ImageModal show={this.state.showImageModal} hideImageModal={this.hideModalHandler} image={this.state.selectedImage}/> : null}
         <div className={classes["image-grid"]}>
           {images1}
         </div>
--- src/components/Unsplash/ImageModal/ImageModal.js
+++ src/components/Unsplash/ImageModal/ImageModal.js
@@ -5,13 +5,7 @@ import classes from './ImageModal.module.css';
 
 class ImageModal extends Component {
 
-    state = {
-        loading: true
-    }
-    imageLoadedHandler = () => {
-        console.log("yassshh")
-        this.setState({loading: false})
-    }
+  
     render(){
         return (
             <Modal show={this.props.show} hideModal={this.props.hideImageModal} >
@@ -23,16 +17,30 @@ class ImageModal extends Component {
                         <p>Yash</p>
                         <p>Singh</p>
                     </div>
+                    
+                    
+                    
                     <div className={classes["actions"]}>
-                        Download options
+                        <div className={classes["icons"]}>
+                            <ion-icon name="heart"></ion-icon>
+                        </div>
+                        <div className={classes["icons"]}>
+                            <ion-icon name="add-outline"></ion-icon>
+                        </div>
+                        <div className={classes["download-button"]}>
+                            <a title="Download photo" href={`${this.props.image.links.download}?force=true`} rel="nofollow" target="_blank"  ><span class="_2Aga-">Download</span></a>
+                        </div>
                     </div>  
                 </div>
+
                 <div className={classes["image-container"]}>
-                    <img src={this.props.imageToLoad} alt="india's tricolour flag" onLoad={this.imageLoadedHandler} />
+                    <img src={this.props.image.urls.regular} alt="india's tricolour flag"  />
                 </div>
+
                 <div className={classes["image-footer"]}>
-                    India's tricolour
+                    {this.props.image.description || this.props.image.alt_description}
                 </div>
+
             </Modal>
         )
     }
