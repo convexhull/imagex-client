7c77e77a10b33b2ddc1b5470cc57b1ef9028dadb
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-07-23 00:42:45 +0530

    TYPE:FEATURE -> work on image container part of signup page

--- src/containers/Authentication/Signup/Signup.js
+++ src/containers/Authentication/Signup/Signup.js
@@ -1,6 +1,9 @@
 import React , { Component } from 'react';
 import classes from './Signup.module.css';
 
+import image from '../../../assets/images/signup.jpeg';
+
+
 
 class Signup extends Component {
 
@@ -8,12 +11,15 @@ class Signup extends Component {
         return (
             <div className={classes.Signup}>
                 <div className={classes["image-container"]}>
-                    <h1>Image@</h1>
+                    <img src={image} />
                 </div>
                 <div className={classes["form-container"]}>
-                    <form>
-                        <h1>Form</h1>
-                    </form>
+                    <div className={classes["signup-info"]}>
+                        <h1>Join Unsplash</h1>
+                        <form>
+                            <button>Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         )
