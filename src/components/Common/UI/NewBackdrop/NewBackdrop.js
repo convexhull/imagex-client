b4986c8ad85998215ab6016eb815afe72a2a85aa
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-09-12 16:37:42 +0530

    TYPE:FEATURE -> start working on mobile view for login page, dropdown menu in main navbar

--- src/components/Common/UI/NewBackdrop/NewBackdrop.js
+++ src/components/Common/UI/NewBackdrop/NewBackdrop.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-import classes from './Backdrop.module.css';
+import classes from './NewBackdrop.module.css';
 
 class Backdrop extends Component {
     render(){
@@ -11,7 +11,7 @@ class Backdrop extends Component {
             backdropClasses.push(classes["backdrop--transparent"])
         }
         return (
-            this.props.show ? <div className={backdropClasses.join(' ')} onClick={this.props.hideBackdrop}></div> : null 
+            <div className={backdropClasses.join(' ')} onClick={this.props.hideBackdrop}></div> 
         );
     }
 }
--- src/components/Header/MainNavigation/DropdownMenu/DropdownMenu.js
+++ src/components/Header/MainNavigation/DropdownMenu/DropdownMenu.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import classes from "./DropdownMenu.module.css";
-import Backdrop from "../../../Common/UI/Backdrop/Backdrop";
+import Backdrop from "../../../Common/UI/NewBackdrop/NewBackdrop";
+
 
 class DropdownMenu extends Component {
   render() {
+
+    let authenticationInfo = (
+      <React.Fragment>
+        <li className={classes["link"]}>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li className={classes["link"]}>
+          <NavLink to="/signup">Join free</NavLink>
+        </li>
+      </React.Fragment>
+  );
+
+    if(this.props.isAuthenticated) {
+      authenticationInfo = (
+        <React.Fragment>
+        <li className={classes["link"]}>
+          <NavLink to="/favourite-images">My Favourites</NavLink>
+        </li>
+        <li className={classes["link"]}>
+          <NavLink to="/profile/favourite-images">My Profile</NavLink>
+        </li>
+      </React.Fragment>
+      );
+    }
+
+
     return (
       <React.Fragment>
-        {/* <Backdrop show={true} /> */}
-        <ul className={classes["responsive-topnav"]}>
+        <Backdrop opacity="transparent" hideBackdrop={this.props.toggleDropdownMenu} />
+        <ul className={classes["responsive-topnav"]} onClick={this.props.toggleDropdownMenu} >
           <li className={classes["link"]}>
             <NavLink to="/unsplash">Unsplash</NavLink>
           </li>
@@ -22,10 +50,18 @@ class DropdownMenu extends Component {
           <li onClick={this.props.randomImageLoad} className={classes["link"]}>
             <NavLink to="#">Random Image</NavLink>
           </li>
+          {authenticationInfo}
         </ul>
       </React.Fragment>
     );
   }
 }
 
-export default DropdownMenu;
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(DropdownMenu);
