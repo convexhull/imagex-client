aaefddffd89973dd20d086748b41647c44509aba
Yash Pratap Singh <singhyashpratap06@gmail.com>
2023-07-14 20:05:32 +0530

    Change backend url to https

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://13.53.200.109:4000'
+    baseURL: 'https://api.yashsingh.dev'
 });
 
 export default axiosInstance;
