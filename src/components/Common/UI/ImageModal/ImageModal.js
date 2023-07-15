5fdb32b3a47df0fe55b6604ced842d3f0bc812fd
Yash Pratap Singh <singhyashpratap06@gmail.com>
2023-07-15 21:22:19 +0530

    Convert extract <ImageModal /> to functional component

--- src/components/Common/UI/ImageModal/ImageModal.js
+++ src/components/Common/UI/ImageModal/ImageModal.js
@@ -1,72 +1,68 @@
-import React , { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import Modal from "../Modal/Modal";
+import classes from "./ImageModal.module.css";
+import * as actions from "../../../../store/actions/index";
+import GeneralUtils from "../../../../utils/generalUtils";
+import { useHistory } from "react-router-dom";
 
+const ImageModal = ({ hideImageModal, image }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [liked, setLiked] = useState(false);
+  let imageDescription = image.description || image.alt_description;
+  imageDescription = GeneralUtils.capitalizeFirstLetter(imageDescription);
+  let likeBtnClasses = [classes["icons"]];
+  if (liked) {
+    likeBtnClasses.push(classes["like-btn--liked"]);
+  }
+  const likeBtnHandler = () => {
+    setLiked(true);
+    dispatch(actions.asyncSaveFavouriteImageStart(image, "unsplash", history));
+  };
+  return (
+    <Modal hideModal={hideImageModal}>
+      <div className={classes["image-header"]}>
+        <div className={classes["user-info"]}>
+          <div>
+            <img src={image.user.profile_image.large} alt="uploader's" />
+          </div>
+          <p>
+            <strong>{image.user.name}</strong>
+          </p>
+          <p>@{image.user.username}</p>
+        </div>
+        <div className={classes["actions"]}>
+          <div className={likeBtnClasses.join(" ")} onClick={likeBtnHandler}>
+            <ion-icon name="heart"></ion-icon>
+          </div>
+          <div className={classes["download-button"]}>
+            <a
+              title="Download photo"
+              href={`${image.links.download}?force=true`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <span className="_2Aga-">
+                {" "}
+                <i class="fa fa-download" aria-hidden="true"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={classes["image-container"]}>
+        <img
+          src={image.urls.regular}
+          alt={
+            image.alt_description || image.description || "alternate definition"
+          }
+        />
+      </div>
+      <br />
+      <div className={classes["image-footer"]}>{imageDescription}</div>
+    </Modal>
+  );
+};
 
-import Modal from '../Modal/Modal';
-import classes from './ImageModal.module.css';
-import * as actions from '../../../../store/actions/index';
-import GeneralUtils from '../../../../utils/generalUtils';
-import { withRouter } from 'react-router-dom';
-
-
-class ImageModal extends Component {
-
-    state = {
-        liked: false
-    }
-
-    likeBtnHandler = () => {
-        this.setState({
-            liked: true
-        });
-        this.props.onAddToFavourites(this.props.image, "unsplash", this.props.history);
-    }
-
-    render(){
-        let imageDescription = this.props.image.description || this.props.image.alt_description;
-        imageDescription = GeneralUtils.capitalizeFirstLetter(imageDescription);
-        let likeBtnClasses = [classes["icons"]];
-        if(this.state.liked){
-            likeBtnClasses.push(classes["like-btn--liked"]);
-        }
-        return (
-            <Modal hideModal={this.props.hideImageModal} >
-                <div className={classes["image-header"]}>
-                    <div className={classes["user-info"]}>
-                        <div>
-                            <img src={this.props.image.user.profile_image.large } alt="uploader's" />
-                        </div>
-                        <p><strong>{this.props.image.user.name}</strong></p>
-                        <p>@{this.props.image.user.username}</p>
-                    </div>
-                    <div className={classes["actions"]}>
-                        <div className={likeBtnClasses.join(' ')} onClick={this.likeBtnHandler}>
-                            <ion-icon name="heart"></ion-icon>
-                        </div>
-                        <div className={classes["download-button"]}>
-                            <a title="Download photo" href={`${this.props.image.links.download}?force=true`} rel="noopener noreferrer" target="_blank"  ><span className="_2Aga-">    <i class="fa fa-download" aria-hidden="true"></i>
-</span></a>
-                        </div>
-                    </div>
-                </div>
-                <div className={classes["image-container"]}>
-                    <img src={this.props.image.urls.regular} alt={this.props.image.alt_description || this.props.image.description || 'alternate definition'}  />
-                </div>
-                <br />
-                <div className={classes["image-footer"]}>
-                    {imageDescription}
-                </div>
-            </Modal>
-        )
-    }
-}
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onAddToFavourites: (image, platform, history) => dispatch(actions.asyncSaveFavouriteImageStart(image, platform, history))
-    }
-}
-
-
-export default connect(null, mapDispatchToProps)(withRouter(ImageModal));
+export default ImageModal;
--- src/components/Unsplash/ImageGrid/ImageGrid.js
+++ src/components/Unsplash/ImageGrid/ImageGrid.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 import classes from "./ImageGrid.module.css";
-import ImageModal from '../../Common/UI/ImageModal/ImageModal'
+import ImageModal from "../../Common/UI/ImageModal/ImageModal";
 import * as actions from "../../../store/actions/index";
-import ImageUtils from '../../../utils/imageOrientation';
+import ImageUtils from "../../../utils/imageOrientation";
 
 class ImageGrid extends Component {
   state = {
     showImageModal: false,
     selectedImage: "",
     hoveredOverImageId: "",
-    justLikedImageIds: []
+    justLikedImageIds: [],
   };
 
   imageClickHandler = (image) => {
@@ -24,10 +24,10 @@ class ImageGrid extends Component {
   };
 
   likeBtnHandler = (image) => {
-    this.setState((state,props) => {
+    this.setState((state, props) => {
       return {
-        justLikedImageIds: [...state.justLikedImageIds , image.id]
-      }
+        justLikedImageIds: [...state.justLikedImageIds, image.id],
+      };
     });
     this.props.onAddToFavourites(image, "unsplash", this.props.history);
   };
@@ -46,14 +46,17 @@ class ImageGrid extends Component {
 
   render() {
     let imagesToDisplay = this.props.images.map((image) => {
-      let imgOrientation = ImageUtils.calculateOrientationClass(image.width, image.height);
+      let imgOrientation = ImageUtils.calculateOrientationClass(
+        image.width,
+        image.height
+      );
       let imageOptionsClasses = [classes["image__options"]];
       let likeBtnClasses = [classes["like-btn"]];
       if (image.id === this.state.hoveredOverImageId) {
         imageOptionsClasses.push(classes["image__options--visible"]);
       }
-      if(this.state.justLikedImageIds.includes(image.id)){
-        likeBtnClasses.push(classes["like-btn--liked"])
+      if (this.state.justLikedImageIds.includes(image.id)) {
+        likeBtnClasses.push(classes["like-btn--liked"]);
       }
       return (
         <div
@@ -69,11 +72,10 @@ class ImageGrid extends Component {
           <div
             className={classes["image__overlay"]}
             onClick={() => this.imageClickHandler(image)}
-          >
-          </div>
+          ></div>
           <div className={imageOptionsClasses.join(" ")}>
             <div
-              className={likeBtnClasses.join(' ')}
+              className={likeBtnClasses.join(" ")}
               onClick={() => this.likeBtnHandler(image)}
             >
               <span>
@@ -87,8 +89,9 @@ class ImageGrid extends Component {
                 rel="nofollow noopener noreferrer"
                 target="_blank"
               >
-                <span className="_2Aga-"><i class="fa fa-download" aria-hidden="true"></i>
-</span>
+                <span className="_2Aga-">
+                  <i class="fa fa-download" aria-hidden="true"></i>
+                </span>
               </a>
             </div>
           </div>
@@ -100,7 +103,6 @@ class ImageGrid extends Component {
       <div>
         {this.state.showImageModal ? (
           <ImageModal
-            show={this.state.showImageModal}
             hideImageModal={this.hideModalHandler}
             image={this.state.selectedImage}
           />
@@ -115,7 +117,7 @@ const mapStateToProps = (state) => {
   return {
     images: state.unsplash.images,
   };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -124,4 +126,7 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ImageGrid));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(ImageGrid));
