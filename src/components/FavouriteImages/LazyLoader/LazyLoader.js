1296e70a8a0fc09838b9e67d52bf479a3a86a711
Yash Pratap Singh <singhyashpratap06@gmail.com>
2024-06-11 23:05:02 +0530

    Fix broken spinner

--- src/components/FavouriteImages/LazyLoader/LazyLoader.js
+++ src/components/FavouriteImages/LazyLoader/LazyLoader.js
@@ -1,61 +1,56 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-
-
-import ImageGrid from '../ImageGrid/ImageGrid';
-import Spinner from '../../UI/Spinner/Spinner';
-import * as actions from '../../../store/actions/index';
+import ImageGrid from "../ImageGrid/ImageGrid";
+import AwsSpinner from "../../UI/AwsSpinner/spinner.gif";
+import * as actions from "../../../store/actions/index";
 
 class ScrollLazyLoading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loaderRef = React.createRef();
+  }
 
-    constructor(props){
-        super(props);
-        this.loaderRef = React.createRef();
-    }
-   
-    componentDidMount(){
-        let options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.1
-        }
-        let pagecount = 0;
-        let callback = (entries, observer) => {
-            if(entries[0].isIntersecting){
-                pagecount++;
-                this.props.onSearchByKeyword(this.props.keyword, pagecount);
-            }
-        }
-        let observer = new IntersectionObserver(callback, options);
-        observer.observe(this.loaderRef.current);
-    }
+  componentDidMount() {
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+    let pagecount = 0;
+    let callback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        pagecount++;
+        this.props.onSearchByKeyword(this.props.keyword, pagecount);
+      }
+    };
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(this.loaderRef.current);
+  }
 
-    render(){
-        return (
-            <React.Fragment>
-                <ImageGrid />
-                <div ref={this.loaderRef}>
-                    <Spinner  />
-                </div>
-            </React.Fragment>
-        )
-    }
-
+  render() {
+    return (
+      <React.Fragment>
+        <ImageGrid />
+        <div ref={this.loaderRef}>
+          <img src={AwsSpinner} alt="spinner gif" />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
 
 const mapStateToProps = (state) => {
-    return {
-        images: state.favourites.images
-    }
-}
-
+  return {
+    images: state.favourites.images,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onSearchByKeyword : (keyword, page) => dispatch(actions.unsplashImageSearchByKeyword(keyword, page))
-    }
-}
+  return {
+    onSearchByKeyword: (keyword, page) =>
+      dispatch(actions.unsplashImageSearchByKeyword(keyword, page)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScrollLazyLoading);
